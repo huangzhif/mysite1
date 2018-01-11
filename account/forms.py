47ba4c1f9655552f5dd38de84061067bb4154f67
huangzhif <394320736@qq.com>
2018-01-12 00:03:59 +0800

    博客源码

---
+++ account/forms.py
@@ -0,0 +1,23 @@
+#-*- coding:utf-8 -*-
+from django import forms
+from django.contrib.auth.models import User
+
+class LoginForm(forms.Form):
+    """用于存放各种与表单有关的类"""
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+#注册方法
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(label="Password",widget=forms.PasswordInput)
+    password2=forms.CharField(label="Confirm Password",widget=forms.PasswordInput)
+
+    class Meta:
+        model=User
+        fields=("username","email")
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("passwords do not match.")
+        return cd['password2']
---
+++ account/views.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login  # django内置用户认证方法
+from .forms import LoginForm, RegistrationForm
+
+from django.shortcuts import render
+
+
+# Create your views here.
+def user_login(request):
+    if request.method == "POST":  # 登录用户密码认证
+        login_form = LoginForm(request.POST)  # 获取到用户名密码
+        if login_form.is_valid():  # 判断前端提交到后端的数据是否符合表单类属性要求
+            # cleaned_data是实例的属性，它以字典形式返回实例的具体数据，即经过检验之后的属性及其值。
+            # 如果传入的某项数据不合法，则在cleaned_data的结果中不予显示
+            cd = login_form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+
+            if user:
+                login(request, user)
+                return HttpResponse("Welcome you.you have been authenticated successfully")
+            else:
+                return HttpResponse("Invalid login")
+
+    if request.method == "GET":  # 打开网站
+        login_form = LoginForm()  # 第一次访问网站，获取未绑定数据的表单实例
+        return render(request, "account/login.html", {"form": login_form})
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = RegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return HttpResponse("successfully")
+        else:
+            return HttpResponse("sorry,you can not register.")
+
+    else:
+        user_form = RegistrationForm()
+        return render(request, "account/register.html", {"form": user_form})
