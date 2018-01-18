0a355ece591a0fc554680dec04a9e9dfc58fc68f
huangzhif <394320736@qq.com>
2018-01-18 22:44:53 +0800

    栏目管理

--- mysite1/urls.py
+++ mysite1/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.conf.urls import url ,include
+from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/',include('blog.urls',namespace='blog',app_name='blog')),
-    url(r'^account/',include('account.urls',namespace='account',app_name='account')),
+    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
+    url(r'^account/', include('account.urls', namespace='account', app_name='account')),
+    url(r'^article/', include('article.urls', namespace='article', app_name="article")),
 ]
