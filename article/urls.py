6ca4d2a46f4ff9b7b56924b0df363314c4911be4
huangzhif <394320736@qq.com>
2018-01-24 00:13:18 +0800

    删除，修改与分页

--- article/urls.py
+++ article/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     url(r'^del-column/$', views.del_article_column, name='del_article_column'),
     url(r'^article-post/$', views.article_post, name="article_post"),
     url(r'^article-list/$', views.article_list, name="article_list"),
-    url(r'^article-detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.article_detail, name="article_detail")
+    url(r'^article-detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.article_detail, name="article_detail"),
+    url(r'^del_article/$', views.del_article, name="del_article"),
+    url(r'^redit-article/(?P<article_id>\d+)/$', views.redit_article, name="redit_article"),
 ]
